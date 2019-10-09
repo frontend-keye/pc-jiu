a23da6c2ca9773934f2b16753111b6f3f358ba75
wink <wink@winkdeMacBook-Pro.local>
2019-10-09 17:33:39 +0800

    Updates

---
+++ precache-manifest.d1c509492ae7df50b1fe77c9b2639529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af78567a2b13eb6c79c7142120855c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abeece93abd08b1b691c",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d71c70221ad1f708ca27",
+    "url": "./static/js/2.979fc60c.chunk.js"
+  },
+  {
+    "revision": "abeece93abd08b1b691c",
+    "url": "./static/js/main.4e0e8abe.chunk.js"
+  },
+  {
+    "revision": "e01e9e9e3abc03a2378a",
+    "url": "./static/js/runtime-main.ee8783e7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
