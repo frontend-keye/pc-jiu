dddfa13e6cb66c1f1f9926e6e4e91d1552186e9d
wink <wink@winkdeMBP.lan>
2019-11-22 14:48:15 +0800

    头部

--- src/page/compony/index.js
+++ src/page/compony/index.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class Compony extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      index: 1
+    }
   }
   render() { 
     return ( 
       <div className="compony">
-        Compony
+        <div className="left-bar">
+          <p className="title-bar">企業情報</p>
+          <p>会社概要</p>
+          <p>調達方</p>
+          <p>CSR: 企業行動憲章</p>
+        </div>
       </div>
     );
   }
